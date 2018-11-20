c9d7e533361e5996e046f3ce7187736353d9f9ca
Joker <chenhua385148231@126.com>
2018-11-20 11:27:42 +0800

    citySelect

---
+++ src/views/CitySelect/citySelect.vue
@@ -0,0 +1,29 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: 'citySelect',
+  data(){
+    return {
+    }
+  },
+  props: {
+  },
+  components: {
+  },
+  created(){
+  },
+  computed: {
+  },
+  mounted(){
+  },
+  methods:{
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
