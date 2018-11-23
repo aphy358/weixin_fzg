30b055dcb9a4562479587301609ffd6f9c8bd804
clou <464925989@qq.com>
2018-11-23 11:29:04 +0800

    添加微笑信支持

--- src/plugins/vue-wechat-plugin/wechatAuth.js
+++ src/plugins/vue-wechat-plugin/wechatAuth.js
@@ -14,8 +14,8 @@ class wechatAuth {
 
     // 调取微信获取code接口
     getCode() {
-        let authPageBaseUri = 'https://open.weixin.qq.com/connect/oauth2/authorize';
-        let authParams = `?appid=${this.config.appid}&redirect_uri=${this.config.redirectUri}&response_type=${this.config.responseType}&scope=${this.config.scope}#wechat_redirect`;
+        let authPageBaseUri = 'https://open.weixin.qq.com/connect/oauth2/authorize'
+        let authParams = `?appid=${this.config.appid}&redirect_uri=${this.config.redirectUri}&response_type=${this.config.responseType}&scope=${this.config.scope}#wechat_redirect`
         window.location.href = authPageBaseUri + authParams;
     }
 
@@ -23,17 +23,17 @@ class wechatAuth {
         return (to, user) => {
             if (user) {
                 window.sessionStorage.setItem('user', user);
-                to ? next(to) : next();
+                to ? next(to) : next()
             } else {
-                to && next(to);
+                to && next(to)
             }
         }
     }
 
     getCodeCallback(next, code) {
-        return this.config.getCodeCallback(this.next(next), code);
+        return this.config.getCodeCallback(this.next(next), code)
     }
 
 }
 
-export default wechatAuth;
+export default wechatAuth
