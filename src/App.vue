49c6d7bb8809e2bc3afc2a1da4965c4be5b0beb5
Joker <chenhua385148231@126.com>
2018-12-22 14:31:54 +0800

    f

--- src/App.vue
+++ src/App.vue
@@ -67,7 +67,28 @@ export default {
     }
   },
   components: {},
-  created() {},
+  created() {
+    // 初始化 JSSDK
+    this.$api.common.syncInitJSSDK({url: encodeURIComponent(location.href.split('#')[0])}).then(res => {
+      if(res.returnCode == 1){
+        let data = res.data
+
+        wx.config({
+          debug : false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+          appId : data.appId, // 必填，公众号的唯一标识
+          timestamp : data.timestamp, // 必填，生成签名的时间戳
+          nonceStr : data.nonceStr, // 必填，生成签名的随机串
+          signature : data.signature,// 必填，签名，见附录1
+          jsApiList : ['updateAppMessageShareData', 'updateTimelineShareData', 'openLocation'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+        });
+      }
+    })
+
+    let _this = this
+    wx.ready(function(){
+      _this.$store.commit('setCommonState', {k: 'wx_ready', v: true})
+    });
+  },
   computed: {},
   mounted() {},
   methods: {}
