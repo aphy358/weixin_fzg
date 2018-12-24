4b0336a4c738d261acbb7eb6b1b94c01442060a1
Tsingwu <1211751992@qq.com>
2018-12-24 16:21:31 +0800

    hotelOrderList

---
+++ src/views/hotelOrderList/hotelOrderList.vue
@@ -0,0 +1,27 @@
+<!-- 组件说明 -->
+<template>
+	<div class="hotel-order-list">
+		<mt-header title="我的订单"></mt-header>
+	</div>
+</template>
+
+<script>
+  export default {
+    name: '',
+    
+    data() {
+      return {}
+    },
+    
+    props: {},
+    
+    components: {},
+    
+    computed: {},
+    
+    methods: {}
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
