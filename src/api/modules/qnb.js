b17a30185de9f7242b2de7cff6dbc4709e15cc7c
Joker <chenhua385148231@126.com>
2018-12-27 16:14:40 +0800

    f

--- src/api/modules/qnb.js
+++ src/api/modules/qnb.js
@@ -26,6 +26,10 @@ export const syncQNBHasAuthority = (args, loading = false) => http.post("/vue/qn
 export const syncQNBCancleConcern = (args, loading = false) => http.post("/vue/qnbPersonalCenter/cancleAttention.do", args, loading)
 
 
+// 增加对某酒店供应商的关注
+export const syncQNBAddConcern = (args, loading = false) => http.post("/vue/qnbPersonalCenter/addAttenton.do", args, loading)
+
+
 // 查询某个酒店下的所有供应商
 export const syncQNBQuerySuppliers = (args, loading = true) => http.post("/vue/qnbPersonalCenter/hotelItemList.do", args, loading)
 
--- src/views/QNB/QNBSupplierList.vue
+++ src/views/QNB/QNBSupplierList.vue
@@ -7,7 +7,21 @@
     <!-- 返回上一页 -->
     <GoBack _style="top: 0.02rem" />
     
-    <div class="page-content" style="background-color: #efeff4;">
+    <div class="page-content" style="background-color: #efeff4;padding-bottom: 0.4rem;">
+
+      <section class="supplier-list-KWI-outer">
+        <select name="" v-model="countryType">
+          <option value="0">境内</option>
+          <option value="2">港澳台</option>
+          <option value="3">境外</option>
+        </select>
+
+			  <div class="supplier-list-KWI-wrap">
+          <i class="iconfont icon-search0" style="margin-left: 0.05rem;"></i>
+          <input type="text" class="keyw-input" placeholder="酒店关键字" v-model="keyword" >
+          <i v-show="keyword" class="iconfont icon-shanchu" @click="clearKeyWord"></i>
+        </div>
+      </section>
 
       <ul 
         class="qnb-hotel-list"
@@ -20,15 +34,19 @@
             <span class="info-name">{{ n.n }}</span>
             <i class="iconfont" :class="[n.expand ? 'icon-down-thin' : 'icon-right-thin']"></i>
           </div>
-          <ul class="supplier-list" v-if="n.expand && n.supplierList && n.supplierList.length > 0">
-            <li class="supplier-item line-after" v-for="(o, j) in n.supplierList" :key="j" @click="clickOneSupplier(o)">
-              {{ o.suppName }}
-              <i v-if="o.hasAttention" class="iconfont icon-gouxuan"></i>
-            </li>
-          </ul>
-          <ul class="supplier-list" v-if="n.expand && n.supplierList && n.supplierList.length < 1">
-            <li class="supplier-item line-after" style="color: #999;">无供应商</li>
-          </ul>
+
+          <transition @enter="enter" @leave="leave">
+            <ul class="supplier-list" v-if="n.expand && n.supplierList && n.supplierList.length > 0">
+              <li class="supplier-item line-after" v-for="(o, j) in n.supplierList" :key="j" @click="clickOneSupplier(o, n)">
+                {{ o.suppName }}
+                <i v-if="o.hasAttention" class="iconfont icon-gouxuan"></i>
+              </li>
+            </ul>
+            <ul class="supplier-list" v-if="n.expand && n.supplierList && n.supplierList.length < 1">
+              <li class="supplier-item line-after" style="color: #999;">无供应商</li>
+            </ul>
+          </transition>
+
         </li>
         
       </ul>
@@ -51,6 +69,7 @@ import LoadMore from '@/components/LoadMore.vue'
 import END from '@/components/END.vue'
 import { gotoPage, replacePage, queryString } from '@/assets/util'
 import { debounce } from 'lodash'
+import Velocity from "velocity-animate";
 
 import { _hotelList } from './hotelList.js'
 
@@ -63,12 +82,13 @@ export default {
       // 1：用于添加关注   2：直接由供应商页面进入到房型列表页
       pageType: '',
 
+      countryType: 0,
+
       hotelList: [],
       loading: false,
       infiniteLoad: false,
       pageNow: 1,
-
-      iterator: 1,
+      keyword: ''
     }
   },
   props: {},
@@ -78,7 +98,16 @@ export default {
     LoadMore,
     END
   },
-  watch: {},
+  watch: {
+    // 监控输入关键字的变化，并查询酒店列表
+    keyword(){
+      this.getHotelListDebounce(1)
+    },
+    // 监控地区的变化（境内、港澳台、境外），并查询酒店列表
+    countryType(){
+      this.getHotelList(1)
+    }
+  },
   created(){
     this.getHotelList(1)
   },
@@ -90,18 +119,13 @@ export default {
   computed: {},
   mounted(){},
   methods:{
-    // 跳转到房型列表页，要先验证用户有没有操作权限
-    gotoRoomListPage(n){
-      let params = {infoId: n.infoId, suppId: n.suppId}
-      this.$api.qnb.syncQNBHasAuthority(params).then(res => {
-        if(res.returnCode === 1){
-          // TO DO
-        }
-      })
-    },
     // 获取 url 参数
     getQueryParams(){
       this.pageType = queryString('pageType')
+    },
+    // 清空关键字
+    clearKeyWord(){
+      this.keyword = ''
     },
     // 获取酒店列表
     getHotelListDebounce: debounce(function(flag){
@@ -109,6 +133,8 @@ export default {
     }, 290),
     // 获取酒店列表回调函数
     getHotelList: debounce(function(flag){
+      console.log('getHotelList');
+      
       // 如果已经在查询酒店列表，则暂时不允许再查询
       if(this.loading)   return false
       this.loading = true
@@ -118,9 +144,9 @@ export default {
       }
 
       let params = {
-        pageSize: 15,
+        pageSize: 10,
         pageNum: this.pageNow,
-        countryType: 0,
+        countryType: this.countryType,
         key: '',
       }
 
@@ -144,10 +170,10 @@ export default {
       if(!n.supplierList){
         this.$api.qnb.syncQNBQuerySuppliers({hotelId: n.i}).then(res => {
           n.supplierList = [
-            {suppId: '1', suppName: '供应商' + this.iterator++, hasAttention: false},
-            {suppId: '1', suppName: '供应商' + this.iterator++, hasAttention: false},
-            {suppId: '1', suppName: '供应商' + this.iterator++, hasAttention: false},
-            {suppId: '1', suppName: '供应商' + this.iterator++, hasAttention: true},
+            {suppId: '1', suppName: '供应商', hasAttention: false},
+            {suppId: '1', suppName: '供应商', hasAttention: false},
+            {suppId: '1', suppName: '供应商', hasAttention: false},
+            {suppId: '1', suppName: '供应商', hasAttention: true},
           ]
           n.expand = !n.expand
 
@@ -159,15 +185,82 @@ export default {
         n.expand = !n.expand
       }
     },
-    // 单击某个供应商，或关注，或跳转到房型列表页面
-    clickOneSupplier(o){
-
+    // 跳转到房型列表页，要先验证用户有没有操作权限
+    gotoRoomListPage(n){
+      let params = {infoId: n.infoId, suppId: n.suppId}
+      this.$api.qnb.syncQNBHasAuthority(params).then(res => {
+        if(res.returnCode === 1){
+          // TO DO
+        }
+      })
+    },
+    // 添加关注
+    addConcernSupplier(n){
+      let params = {infoId: n.infoId, suppId: n.suppId}
+      this.$api.qnb.syncQNBAddConcern(params).then(res => {
+        if(res.returnCode === 1){
+          // TO DO
+        }
+      })
+    },
+    // 单击某个供应商，或关注，或跳转到房型列表页面 // 1：用于添加关注   2：直接由供应商页面进入到房型列表页
+    clickOneSupplier(o, n){
+      if(this.pageType == 2){
+        this.gotoRoomListPage({infoId: n.i, suppId: o.suppId})
+      }else{
+        if(o.hasAttention){
+          Toast('已关注该供应商！')
+				  return true
+        }
+        this.addConcernSupplier({infoId: n.i, suppId: o.suppId})
+      }
+    },
+    enter: function(el, done) {
+      Velocity(el, 'slideDown', { duration: 200, complete: done });
+    },
+    leave: function(el, done) {
+      Velocity(el, 'slideUp', { duration: 200, complete: done });
     }
   }
 }
 </script>
 
 <style lang="scss">
+.supplier-list-KWI-outer{
+  margin: 0.1rem;
+  overflow: hidden;
+
+  select{
+    float: left;
+    width: 0.52rem;
+    padding: 0 0.05rem;
+    border: none;
+    border-radius: 0.02rem;
+    line-height: 0.4rem;
+    box-shadow: 0 0 0.05rem 0 rgba(0, 0, 0, 0.1);
+  }
+
+  @at-root .supplier-list-KWI-wrap{
+    position: relative;
+    float: right;
+    width: calc(100% - 0.6rem);
+    background: white;
+    border-radius: 0.02rem;
+    line-height: 0.4rem;
+
+    .iconfont{
+      color: #ccc;
+    }
+
+    input{
+      padding: 0 0.05rem;
+      border: none;
+      width: calc(100% - 0.6rem);
+    }
+
+  }
+}
+
 .qnb-hotel-list{
 
   @at-root .qnb-hotel-item{
@@ -178,7 +271,7 @@ export default {
 
     @at-root .qnb-hi-title-wrap{
       position: relative;
-      padding: 0.1rem;
+      padding: 0.15rem 0.1rem;
       font-size: 0.15rem;
 
       .info-name{
@@ -187,10 +280,10 @@ export default {
 
       .iconfont{
         position: absolute;
+        top: 0.18rem;
         right: 0.05rem;
         font-size: 0.12rem;
         color: #ccc;
-
       }
     }
 
@@ -198,12 +291,12 @@ export default {
       padding: 0 0.1rem 0.1rem 0.3rem;
 
       .supplier-item{
-        position: relative;padding: 10px 0;
+        position: relative;padding: 0.1rem 0;
 
         .icon-gouxuan{
           position: absolute;
           right: 0;
-          top: 9px;
+          top: 0.09rem;
           color: green;
         }
       }
