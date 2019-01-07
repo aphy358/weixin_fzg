5272f922b6a64215e547c2398908ec3dfe56cc5d
Joker <chenhua385148231@126.com>
2019-01-07 15:50:37 +0800

    f

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import axios from 'axios'
+import api from "../api"
 import wechatPlugin from '@/plugins/vue-wechat-plugin'
 import Home from '@/views/Home/home.vue'
 import ebRouter from './ebRouter'
@@ -124,7 +124,7 @@ Vue.use(Router)
 // 微信授权插件初始化
 Vue.use(wechatPlugin, {
   router, // 路由实例对象
-  appid: 'wx41041c8613e4b4b0', // 测试appid： wxdc97f923fbec8173
+  appid: 'wx41041c8613e4b4b0', // 测试appid： wxdc97f923fbec8173    趣程appid： wx41041c8613e4b4b0
   responseType: 'code', // 返回类型，请填写code
   scope: 'snsapi_userinfo', // 应用授权作用域，snsapi_base （不弹出授权页面，直接跳转，只能获取用户openid），snsapi_userinfo （弹出授权页面，可通过openid拿到昵称、性别、所在地。并且，即使在未关注的情况下，只要用户授权，也能获取其信息）
   // redirectUri: 'http://aphy358.natapp1.cc', //微信回调地址http://aphy358.natapp1.cc
@@ -136,16 +136,12 @@ Vue.use(wechatPlugin, {
     // next说明：next方法接收两个参数
     // 参数1(必填，切换到的路由地址，空字符串为当前路由，指定切换对象 next('/') 或者 next({ path: '/' })
     // 参数2为通过code值请求后端获取access_token的结果，true或者false，默认为false
-    axios.get('/autoLoginWx.do', {//code  ---  openid ---  user    // 通过code值换取access_token后端接口地址
-      params: {
-        code,
-        state: ''
-      },
-    }).then(response => {
-      console.log(pageType);
-      
-      if(response.status == 200 && response.data.returnCode == 1){
-        let data = response.data.data
+    // code  ---  openid ---  user    // 通过code值换取access_token后端接口地址
+    // axios.post('/vue/autoLoginWx.do', { code: code, state: '' }).then(response => {
+    api.common.syncAuthLogin({ code: code, state: '' }).then(response => {
+      if(response.returnCode == 1){
+        debugger
+        let data = response.data
         let user_wx = data.customerUser
         let user_eb = data.supCustomerUser
         let user_qnb = data.qnbUser
@@ -155,6 +151,7 @@ Vue.use(wechatPlugin, {
         if(user_qnb)   window.sessionStorage.setItem('user_qnb', JSON.stringify(user_qnb))
 
         if(pageType == 2){
+          alert(JSON.stringify(user_eb))
           user_eb ? next() : next('/ebLogin')
         }else if(pageType == 3){
           user_qnb ? next() : next('/qnblogin')
@@ -162,7 +159,8 @@ Vue.use(wechatPlugin, {
           user_wx ? next() : next('/login')
         }
       }
-    }).catch(() => {
+    }).catch((e) => {
+      alert('catch' + e)
       next('/login'); // ajax出现错误
     })
   },
