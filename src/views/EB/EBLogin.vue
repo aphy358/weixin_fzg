c4ea2f53b18ae8cf2a954de7dfdc9d1bd8d89812
Joker <chenhua385148231@126.com>
2019-01-14 14:27:33 +0800

    f

--- src/views/EB/EBLogin.vue
+++ src/views/EB/EBLogin.vue
@@ -76,15 +76,15 @@ export default {
   created(){
 
     //*** 模拟登录，测试 */
-    let params = {
-      code: '8998',
-      distrbCode: 'MZ140111',
-      name: 'HJM1',
-      password: '1'
-    }
-    this.$api.eb.syncEBLoginForTest(params).then(res => {
-      console.log(res);
-    })
+    // let params = {
+    //   code: '8998',
+    //   distrbCode: 'MZ140111',
+    //   name: 'HJM1',
+    //   password: '1'
+    // }
+    // this.$api.eb.syncEBLoginForTest(params).then(res => {
+    //   console.log(res);
+    // })
 
 
     this.logo = logo
