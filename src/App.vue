02c8229d3d7cd82a5e64e7c5a6a02c8e29eb9565
Joker <chenhua385148231@126.com>
2019-01-23 19:27:55 +0800

    f

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,12 @@ export default {
       if(to.path.toLowerCase() != '/hoteldetail'){
         this.initPageShare()
       }
+
+      // 解决部分 iphone 换页的时候，软键盘导致页面向上顶起而不收回的 bug，但如果不换页的情况下也执行 window.scroll(0, 0) 的话，就会在多个input框之间切换的情况下出现上下闪动的 bug，所以这里需要一个全局变量控制
+      window.changePage = true
+      setTimeout(function(){
+        window.changePage = false
+      }, 100)
     }
   },
   components: {},
@@ -37,8 +43,17 @@ export default {
     // 解决部分 iphone 换页的时候，软键盘导致页面向上顶起而不收回的 bug
     document.body.addEventListener('focusout', () => { //软键盘关闭事件
       setTimeout(function(){
-        window.scroll(0, 0)
+        if(window.changePage)(
+          window.scroll(0, 0)
+        )
       }, 10)
+    })
+
+    // 解决安卓手机下点击输入框时，页面不随之向上滚动，导致弹起的软键盘遮住输入框的问题
+    window.addEventListener("resize", function() {
+      if(document.activeElement.tagName === "INPUT" || document.activeElement.tagName === "TEXTAREA") {
+        document.activeElement.scrollIntoViewIfNeeded()
+      }
     })
   },
   computed: {},
